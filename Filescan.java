12e18828a73c8395e0802b2540c686944744dff8
angela101010 <45670669+angela101010@users.noreply.github.com>
2019-06-11 11:01:17 -0700

    Add files via upload

--- Filescan.java
+++ Filescan.java
@@ -7,25 +7,27 @@ class Filescan{
         File f = new File("quiz answers.csv");
         try {
             Scanner input = new Scanner(f);
-            
+            String[] key = null;
+            input.nextLine();
             while(input.hasNext()){
                 String str = input.nextLine();
-                String[]value = str.split(",");
+                String[] value = str.split(",");
+                
                 if(value[0].equals("Answer Key")) {
-                  String[]key = value;
+                    key = value;
+                    
                 }
-                  int grade = 0;
-                  for(int i = 1; i<key.length; i++) {
-                      if(value[i].equals(key[i])){
-                         grade++;
-                        }
+                   int grade = 0;
+                for(int i = 1; i< value.length; i++) {
+                    if(value[i].equals(key[i])){
+                        grade++;
                     }
-                  System.out.println(value[0]);  
-                  System.out.print(grade+"/8");
-                
                 }
-                
-            
+                System.out.println(value[0]+":");  
+                System.out.println(grade+"/8");
+
+            }
+
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
